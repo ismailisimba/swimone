8499b6da5945c5944cddc1966626ad4243291a65
ismizo <nerdyjinnyas@gmail.com>
2021-01-30 14:06:14 +0300

    mmm

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ function onSignIn(googleUser) {
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-  console.log('Token: ' + googleUser.getAuthResponse().access_token);
+  console.log('Token: ' + googleUser.getAuthResponse().id_token);
 }
 
 
