0c0208c6fbc0e5db7bbb0882e4a38ebbc1b7423c
ismizo <nerdyjinnyas@gmail.com>
2021-02-05 20:42:05 +0300

    adj2

--- script.js
+++ script.js
@@ -530,7 +530,7 @@ function expandTheCpan(sumOfHeight){
   alert(height);
   height = parseInt(height,10);
 
-  if(height>50&&height<80){
+  if(height>50&&height<=120){
     sumOfHeight = sumOfHeight + height;
   }
 
