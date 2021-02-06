300f1ce585cc16e65d9bd698e9fc40f81d65a6da
ismizo <nerdyjinnyas@gmail.com>
2021-02-06 10:06:22 +0300

    idconstr try1

--- script.js
+++ script.js
@@ -823,6 +823,7 @@ if(context==="images"){
 
     tempObj.data = data[i].fileData;
     tempObj.name =  data[i].fileInfo.ogname;
+    tempObj.id =  myDate+"---"+hour+minute+second+"---"+ogname;
     tempObj.info.push({"mime":data[i].fileInfo.meme});
     tempObj.info.push({"timeObj":[{"myDate":myDate},{"hour":hour},{"minute":minute},{"second":second}]});
 
