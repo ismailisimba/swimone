98ab21ef5dd14b2b0998a14c2d8a46022eeb4c24
ismizo <nerdyjinnyas@gmail.com>
2021-02-10 17:38:46 +0300

    path 1 ani try

--- script.js
+++ script.js
@@ -286,6 +286,7 @@ myCanvas.style.fontSize = "14px";
 myCanvas.style.fontWeight = "600";
 myCanvas.style.letterSpacing = "2px";
 myCanvas.style.display = "flex";
+popUp.remove();
 
 if(loginStatus==="captainHasTheCon"){
   initSetupBackend("captainHasTheCon");
