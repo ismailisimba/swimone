77d2365d3f8802a4dfb29f0bef2b72e0e54243c4
ismizo <nerdyjinnyas@gmail.com>
2021-02-11 07:30:55 +0300

    adding loading phrase

--- script.js
+++ script.js
@@ -1089,12 +1089,16 @@ function sendDeletionsToServer () {
 
 function customPopUpFunc(popupEle,phrase,action) {
 
+  let mom = document.querySelectorAll(".spanCont")[0];
+  let deKid = mom.querySelectorAll("span")[0]
+
   if(action==="stop"){
     popupEle.style.visibility = "collapse";
     popupEle.remove();
 
   }else if(action==="fullsteamahead"){
     popupEle.style.visibility = "visible";
+    deKid.innerText = phrase;
     document.querySelectorAll(".mycolumns")[1].appendChild(popupEle);
   }
 
