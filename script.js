d756af0fdf6031849aec324d51594bdee54ace1c
ismizo <nerdyjinnyas@gmail.com>
2021-02-12 12:00:24 +0300

    antry

--- script.js
+++ script.js
@@ -678,6 +678,7 @@ function addSiteMapFuncs(cPanItem){
 
 
 function siteMapShowEditors(cPanItem){
+  cPanItem.style.height = "auto";
   cPanItem.innerText = "Please review all the entries then click Save - at bottom os SiteMap Box - when done.";
   let siteMapCont = document.querySelectorAll(".bigcontrolpanelcont")[0];
   let myTextAreas = siteMapCont.querySelectorAll("textarea");
