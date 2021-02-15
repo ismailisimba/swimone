6363d3b60004ac4b8c669b8d745d188ce8b5db7e
ismizo <nerdyjinnyas@gmail.com>
2021-02-15 13:16:36 +0300

    cleanup and add descr textar

--- script.js
+++ script.js
@@ -1322,6 +1322,7 @@ function deStoryFunc(storyObj){
 
    localVar.cloudObj.contentObj.contentObj.draft.stories[0].storyObj = newStoryObj;
    localVar.cloudObj.contentObj.contentObj.draft.stories[0].title = document.getElementById("editposttit").value;
+   localVar.cloudObj.contentObj.contentObj.draft.stories[0].description = document.getElementById("descrtit").value;
    localVar.cloudObj.contentObj.contentObj.draft.stories[0].type = "draft";
 
     let token = getToken();
